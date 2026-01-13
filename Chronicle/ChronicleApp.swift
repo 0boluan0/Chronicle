8381019a8137c441b1a1e1488991bf2e5f3914ec
boluangit config --global user.email 1765487069@qq.com <1765487069@qq.com>
2026-01-13 17:23:17 +0800

    Initial Commit

---
+++ Chronicle/ChronicleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChronicleApp.swift
+//  Chronicle
+//
+//  Created by 冯一航 on 2026/1/13.
+//
+
+import SwiftUI
+
+@main
+struct ChronicleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chronicle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chronicle
+//
+//  Created by 冯一航 on 2026/1/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
