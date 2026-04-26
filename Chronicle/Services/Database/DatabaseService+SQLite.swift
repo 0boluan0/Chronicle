23571b8ee7356a7d63ef2c5d1c46d329f3718d68
boluan <1765487069@qq.com>
2026-04-26 18:04:23 +0800

    fix: use managed sqlite text bindings

--- Chronicle/Services/Database/DatabaseService+SQLite.swift
+++ Chronicle/Services/Database/DatabaseService+SQLite.swift
@@ -8,7 +8,11 @@ import SQLite3
 
 // MARK: - SQLite Helpers
 
-private let sqliteTransientTextDestructor = unsafeBitCast(-1, to: sqlite3_destructor_type.self)
+private func sqliteDestroyManagedText(_ pointer: UnsafeMutableRawPointer?) {
+    sqlite3_free(pointer)
+}
+
+private let sqliteManagedTextDestructor: sqlite3_destructor_type = sqliteDestroyManagedText
 
 extension DatabaseService {
     func execute(sql: String) throws {
@@ -39,15 +43,19 @@ extension DatabaseService {
         guard byteCount <= Int(Int32.max) else {
             throw DatabaseError.bindFailed("SQLite text binding is too large", sql: sql)
         }
-        let result = utf8Bytes.withUnsafeBufferPointer { buffer in
-            sqlite3_bind_text(
-                statement,
-                index,
-                buffer.baseAddress,
-                Int32(byteCount),
-                sqliteTransientTextDestructor
-            )
+        guard let copiedValue = sqlite3_malloc64(UInt64(utf8Bytes.count)) else {
+            throw DatabaseError.bindFailed("Unable to allocate SQLite text binding", sql: sql)
         }
+        utf8Bytes.withUnsafeBufferPointer { buffer in
+            copiedValue.copyMemory(from: buffer.baseAddress!, byteCount: utf8Bytes.count)
+        }
+        let result = sqlite3_bind_text(
+            statement,
+            index,
+            copiedValue.assumingMemoryBound(to: CChar.self),
+            Int32(byteCount),
+            sqliteManagedTextDestructor
+        )
         try bind(sql: sql, result: result, detail: detail)
     }
 
